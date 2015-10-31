eece444b52c1020e9a3bb801c32e0431a057903f
Harry He <hehaoqian@live.com>
2015-10-31 15:58:59 -0700

    Small update to benchmark.sh

--- benchmark.sh
+++ benchmark.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -x
-rm corpus/*.gz
-for f in corpus/*; do { time pigz/pigz -k -9 $f; rm -f corpus/$f.gz; }; done
+rm -f corpus/*.gz
+for f in corpus/*; do { time pigz/pigz -f -k -9 $f; rm -f corpus/$f.gz; }; done
