98e1276c0b18283b3485fc6867751244e41eaff7
Harry He <hehaoqian@live.com>
2015-11-03 18:29:30 -0800

    Make the benchmark script very verbose. Now ouputs many useful stuffs. Do benchmarks for pigz-9, pigzn-9, pigz-11, pignz-11

--- benchmark.sh
+++ benchmark.sh
@@ -1,4 +1,54 @@
 #!/bin/bash
-set -x
+
+filter='! $f == '"*Makefile"
+function benchmark {
+  
+  echo
+  echo "***********************************"
+  echo  $PROMPT
+  echo "***********************************"
+  echo
+
+  for f in $FILES; do \
+    if [[ $filter ]]; then
+      echo
+      echo current file: $f
+      sizebefore=$(echo "scale=2;$(stat -c "%s" $f)/1000000" | bc)
+      t=$(/usr/bin/time -f "%U" $COMMAND $f 2>&1 >/dev/null )
+      sizeafter=$(echo "scale=2;$(stat -c "%s" $f.gz)/1000000" | bc)
+      ratio=$(echo "scale=2;$sizebefore/$sizeafter" | bc)
+      echo size before:$sizebefore MB
+      echo size after: $sizeafter MB
+      echo time: $t seconds
+      echo ratio: $ratio
+      speed=$(echo "scale=2;""$sizebefore / $t" | bc)
+      echo speed: $speed MB/s
+      rm -f $f.gz
+      echo
+    fi
+  done
+}
+
 rm -f corpus/*.gz
-for f in corpus/*; do { time pigz/pigz -f -k -9 $f; rm -f corpus/$f.gz; }; done
+
+PROMPT="Single threaded pigz -9 benchmarks."
+FILES='corpus/*.big'
+COMMAND='pigz/pigzn -f -k -9'
+benchmark
+
+PROMPT="Multi threaded pigz -9 benchmarks."
+FILES='corpus/*.big'
+COMMAND='pigz/pigz -f -k -9'
+benchmark
+
+PROMPT="Single threaded pigz -11 benchmarks."
+FILES='corpus/*.big'
+COMMAND='pigz/pigzn -f -k -11'
+benchmark
+
+
+PROMPT="Multi threaded pigz -11 benchmarks."
+FILES='corpus/*.big'
+COMMAND='pigz/pigz -f -k -11'
+benchmark
+
