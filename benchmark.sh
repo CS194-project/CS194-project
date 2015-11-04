5078fe734ed6d301e5dc2de0272d2e5ae7639a69
Harry He <hehaoqian@live.com>
2015-11-03 21:40:52 -0800

    benchmark size in bytes

--- benchmark.sh
+++ benchmark.sh
@@ -11,15 +11,15 @@ function benchmark {
   for f in $FILES; do \
       rm -f $f.gz
       echo current file: $f
-      sizebefore=$(echo "scale=2;$(stat -c "%s" $f)/1000000" | bc)
+      sizebefore=$(echo "scale=2;$(stat -c "%s" $f)" | bc)
       t=$(/usr/bin/time -f "%e" $COMMAND $f 3>&2 2>&1 1>&3)
-      sizeafter=$(echo "scale=2;$(stat -c "%s" $f.gz)/1000000" | bc)
+      sizeafter=$(echo "scale=2;$(stat -c "%s" $f.gz)" | bc)
       ratio=$(echo "scale=2;$sizebefore/$sizeafter" | bc)
-      echo size before:$sizebefore MB
-      echo size after: $sizeafter MB
+      echo size before:$sizebefore Bytes
+      echo size after: $sizeafter Bytes
       echo time: $t seconds
       echo ratio: $ratio
-      speed=$(echo "scale=2;""$sizebefore / $t" | bc)
+      speed=$(echo "scale=2;""$sizebefore / $t / 1000000" | bc)
       echo speed: $speed MB/s
       rm -f $f.gz
       echo
